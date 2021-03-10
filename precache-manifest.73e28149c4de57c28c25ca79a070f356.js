df3d5c06d0a237801d177322e36f24b863ea2cde
acer <sld377575@gmail.com>
2021-03-10 18:27:09 +0600

    Updates

---
+++ precache-manifest.73e28149c4de57c28c25ca79a070f356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f75169e40de22b934693f290dc293c",
+    "url": "/mic-deepspeech-test/index.html"
+  },
+  {
+    "revision": "a4ef3bfcff8ee461a9e9",
+    "url": "/mic-deepspeech-test/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c7be2226c689d3ec8efc",
+    "url": "/mic-deepspeech-test/static/js/2.a2265f90.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/mic-deepspeech-test/static/js/2.a2265f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ef3bfcff8ee461a9e9",
+    "url": "/mic-deepspeech-test/static/js/main.30a1d81d.chunk.js"
+  },
+  {
+    "revision": "5431d7dbfcb5bd74b391",
+    "url": "/mic-deepspeech-test/static/js/runtime-main.c34c9b88.js"
+  }
+]);
